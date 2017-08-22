dde177e90d1bc407ce44a0ca49877302911bfbbb
wxwlog <30994008+wxwlog@users.noreply.github.com>
2017-08-22 10:47:51 +0800

    验证下载功能正常

---
+++ Assets/AssetUX/Demo/DemoUpdateAsset/demoUpdateAsset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AssetUX;
+
+public class demoUpdateAsset : MonoBehaviour {
+
+    public MainUpdater mainUpdater;
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        yield return mainUpdater.LoadAllVersionFiles();
+        yield return mainUpdater.UpdateFromRemoteAsset();
+    }	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
